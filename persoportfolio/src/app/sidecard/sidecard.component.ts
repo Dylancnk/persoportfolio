bc01fb9747b7d622fee57b505825ad6e701b47ca
Dylan Cheong <dylancnk_97@hotmail.com>
2021-11-27 15:06:46 +0400

    first commit

---
+++ persoportfolio/src/app/sidecard/sidecard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidecard',
+  templateUrl: './sidecard.component.html',
+  styleUrls: ['./sidecard.component.scss']
+})
+export class SidecardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
