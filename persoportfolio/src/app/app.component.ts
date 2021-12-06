b08165a80d448653ab6bb193ebe049f977450f27
Dylan Cheong <dylancnk_97@hotmail.com>
2021-12-06 21:10:14 +0400

    adjust hosting firebase and add animation to cards

--- persoportfolio/src/app/app.component.ts
+++ persoportfolio/src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    AOS.init();
+  }
   myLinkedin = 'https://www.linkedin.com/in/dylan-cheong-nam-kwong-910516143/';
   myFacebook = 'https://www.facebook.com/ed.cheong.37';
   myGithub = 'https://github.com/Dylancnk';
